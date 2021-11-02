b8b3a0ac03969a6564b937c3c0dbcafd1694c4a7
Chang Shu <cshu1@kent.edu>
2021-11-01 22:56:23 -0700

    Updates

---
+++ precache-manifest.9c631e71a15579da323b2f858fa15544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8214deeb89c0ae9fc31",
+    "url": "/serverless-app/static/css/main.cbb34ee6.chunk.css"
+  },
+  {
+    "revision": "d8214deeb89c0ae9fc31",
+    "url": "/serverless-app/static/js/main.d8214dee.chunk.js"
+  },
+  {
+    "revision": "bc050cb1fdfaddf34e40",
+    "url": "/serverless-app/static/js/1.bc050cb1.chunk.js"
+  },
+  {
+    "revision": "01b8580073d06bd6a560",
+    "url": "/serverless-app/static/js/runtime~main.01b85800.js"
+  },
+  {
+    "revision": "3dda97e55a22dcf8cece87d4859c1b52",
+    "url": "/serverless-app/index.html"
+  }
+];
